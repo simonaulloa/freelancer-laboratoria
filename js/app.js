49bd767fc840f48085ce1bfcfce554e0697cd7a8
simona <simonaulloa@gmail.com>
2017-06-01 10:50:06 -0400

    algo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 /*
- * Archivo principal de JS
+ imagenes OTRA VEZ APARECE ABAJO X LA CTM!!! ARREGLAR
  */
+ (function main(){
+	var boxes = Array.from(document.getElementsByClassName("col-4"));
+	var modal = document.getElementById("col-4-modal");
+	var bodyModal, close, img;
+	boxes.forEach(function(box){
+		box.addEventListener("click", function(){
+			modal.innerHTML = "";
+			bodyModal = document.createElement("div");
+			bodyModal.classList.add("modal-body");
+			bodyModal.innerHTML = box.innerHTML;			
+			modal.appendChild(bodyModal);
+			modal.classList.remove("hide");
+			close = document.createElement("div");
+			close.classList.add("close");
+			img = document.createElement("img");
+			img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+			close.appendChild(img);
+			modal.appendChild(close);
+			close.addEventListener("click",function(){
+				modal.classList.add("hide");
+			});
+		});		
+	});
+})();
